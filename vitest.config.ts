ee6b33d0c73dadcf3037e41afe36b84706a7329d
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-05-08 21:23:48 +0200

    At times like this I wish I was a Garbage Man.

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    coverage: {
+      all: true,
+      include: ["src/**/*"],
+    },
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
+  },
+  
+  
+})
