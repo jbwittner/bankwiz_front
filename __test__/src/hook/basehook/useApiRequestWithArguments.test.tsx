86d61403068e908b4f14d46001de65dd404b4478
Jean-Baptiste WITTNER <44366963+jbwittner@users.noreply.github.com>
2023-05-08 22:07:49 +0200

    Add tests (#8)
    * At times like this I wish I was a Garbage Man.
    
    * I CAN HAZ PYTHON, I CAN HAZ INDENTS
    
    * Removed test case since code didn't pass QA
    
    * bla
    
    * Just committing so I can go home
    
    * Deleted API file
    
    * just trolling the repo
    
    * To be honest, I do not quite remember everything I changed here today. But it is all good, I tell ya.

---
+++ __test__/src/hook/basehook/useApiRequestWithArguments.test.tsx
@@ -0,0 +1,31 @@
+import { useApiRequestWithArguments } from '@/hook/BaseHook';
+import { renderHook } from '@testing-library/react';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+
+describe('useApiRequestWithArguments', () => {
+  const mockApiMethod = vi.fn();
+  const mockRequestObject = { id: 123 };
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should call the API method with the provided request object', async () => {
+    mockApiMethod.mockResolvedValueOnce({ data: { result: true } });
+
+    const { result } = renderHook(() =>
+      useApiRequestWithArguments(mockApiMethod, vi.fn(), {}),
+    );
+
+    result.current.sendRequest(mockRequestObject);
+    expect(mockApiMethod).toHaveBeenCalledWith(mockRequestObject);
+  });
+
+  it('should set the loading state to true while the request is being sent', async () => {});
+
+  it('should set the data and call the success callback if the request is successful', async () => {});
+
+  it('should call the error interpreter and set the error and call the error callback if the request fails with an API error', async () => {});
+
+  it('should call the error callback if the request fails with a non-API error', async () => {});
+});
---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    coverage: {
+      all: true,
+      include: ['src/**/*'],
+      reporter: [['lcov', { projectRoot: './src' }]],
+    },
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
+  },
+});
