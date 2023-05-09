45e85c3028c7f84f938ce721ebaa40a055de1221
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-05-09 14:56:56 +0200

    I am Root. We are Root.

--- __test__/src/components/pageform/PageForm.test.tsx
+++ __test__/src/components/pageform/PageForm.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 
 describe('PageForm', () => {
-  const titlePage = 'My Page';
+  const titlePage = 'My title Page';
   const titleForm = 'My Form';
   const linkHref = '/my-page';
   const linkContent = 'Go to my page';
